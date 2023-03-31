679de9c68f762b45200c9cacbf15a7c567153d17
Shubhranka Varma <c-shubhranka.varma@amagi.com>
2023-03-31 14:00:53 +0530

    Added docs - patch transcoder

--- handlers/patchTranscoder.go
+++ handlers/patchTranscoder.go
@@ -33,6 +33,20 @@ func makeGoStructKey(key string) string {
 	return strings.Title(strings.ReplaceAll(newKey, "_", ""))
 }
 
+// PatchTranscoder is a handler to update a transcoder
+// @description Update a transcoder
+// @host localhost:51000
+// @BasePath /
+// @Router /transcoder [patch]
+// @schemes http
+// @Param input_type query string true "input_type" required=true
+// @Param output_type query string true "output_type" required=true
+// @Success 200 {object} string "Transcoder updated successfully."
+// @Failure 400 {object} string "Please provide output_type and input_type in query parameter." example:"Please provide output_type and input_type in query parameter."
+// @Failure 404 {object} string "Transcoder not found." example:"Transcoder not found."
+// @Failure 500 {object} string "Unable to update the transcoder." example:"Unable to update the transcoder."
+// @Failure 422 {object} string "Unable to pass the request payload." example:"Unable to pass the request payload."
+// @Failure 400 {object} string "Invalid request payload." example:"Invalid request payload."
 func (h *TranscoderHandler) PatchTranscoder(c echo.Context) error {
 
 	// transcoder Variable
